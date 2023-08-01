737a507417978423039a7f1714b3399f4fa39f98
Paulo Hortolani <phchortolani@gmail.com>
2023-07-31 21:05:11 -0300

    ajustes

--- pages/src/components/landing/LandingPage.js
+++ pages/src/components/landing/LandingPage.js
@@ -16,19 +16,32 @@ export default function LandingPage(props) {
                     </h3> */}
                     <h3>
 
-                        Aprenda a <b className="text-secondary">lidar com a ansiedade </b>  para uma vida mais leve!
+                        Aprenda a <b className="text-primary">lidar com a ansiedade </b>  para uma vida mais leve!
                     </h3>
                 </div>
                 <div className="landingImgContent p-2">
-                    <Image
-                        style={{ borderRadius: '1rem' }}
-                        className='cover shadow'
-                        src="/land3.jpg"
-                        alt="Psicóloga Dara Marques"
-                        width={400}
-                        height={480}
-                        loading="lazy"
-                    />
+                    {
+                        props.isMobile && <Image
+                            style={{ borderRadius: '1rem' }}
+                            className='cover shadow'
+                            src="/land3.jpg"
+                            alt="Psicóloga Dara Marques"
+                            width={300}
+                            height={280}
+                            loading="lazy"
+                        />
+                    }
+                    {
+                        !props.isMobile && <Image
+                            style={{ borderRadius: '1rem' }}
+                            className='cover shadow'
+                            src="/land3.jpg"
+                            alt="Psicóloga Dara Marques"
+                            width={400}
+                            height={480}
+                            loading="lazy"
+                        />
+                    }
 
                     {/* 
                     {!props.isMobile ? <Image
@@ -48,15 +61,15 @@ export default function LandingPage(props) {
                     } */}
                     {/*      <img className="landImg" src="/land4.png" /> */}
                 </div>
-                <div className="descLand">
-                    <h4 className="text-muted">
+                <div className="descLand pb-4">
+                    <h4 className="text-muted pt-4">
                         Olá, sou a <b className="text-black">Dara Marques</b>, psicóloga.
                     </h4>
                     <h4 className="text-primary">
                         Vamos conversar?
                     </h4>
 
-                    <div className="btnsLand">
+                    <div className="btnsLand ">
                         {/*  <button className="btn btn-lg btn-primary btn-inverse px-2">Saiba Mais</button> */}
                         <button className="btn-land bg-gradient btn btn-lg btn-primary pulse" onClick={() => (window.open('https://api.whatsapp.com/send?phone=5511978493885', "_blank"))}>Agende uma consulta</button>
                     </div>
